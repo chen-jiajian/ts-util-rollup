0191f6135b7e7613732ad2aa22e09b9e300988a0
jarvis <jarvis@muchenglin.com>
2020-10-20 19:34:18 +0800

    搭建

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+// rollup.config.js
+import json from 'rollup-plugin-json'
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json'
+export default {
+  input: 'src/main.ts',
+  output: [
+    {
+      name: pkg.name,
+      file: pkg.main,
+      format: 'umd'
+    },
+    {
+      file: pkg.common,
+      format: 'cjs'
+    },
+    // {
+    //   file: pkg.module,
+    //   format: 'es'
+    // }
+  ],
+  plugins: [
+    json(),
+    typescript({lib: ["es5", "es6", "dom"], target: "es5"})
+  ]
+};
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import isMobile from './isMobile'
+
+export {
+	isMobile
+}
